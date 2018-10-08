1b6a9bbd4074516f6fc69ad5b7c46191635540cb
emilec <emilec@corp.doctor.com>
2018-10-07 21:24:16 -0700

    partial progress bar refactor

--- src/app/components/ProgressBar.js
+++ src/app/components/ProgressBar.js
@@ -9,12 +9,20 @@ const Text = styled.span`
 `;
 
 const Container = styled.div`
-    width: 80%;
+    width: ${props => props.width};
+    margin: ${props => props.margin};
+`;
+
+const BarWrapper = styled.div`
+    width: 100%;
     margin: 0 auto;
+    height: ${props => props.height}px;
+    max-height: ${props => props.height}px;
 `;
 
 const Bar = styled.div`
     position: relative;
+    height: 100%;
 
     &:first-child {
         transition: 0.4s linear;
@@ -22,51 +30,38 @@ const Bar = styled.div`
     }
 `;
 
-class ProgressBar extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            hello: false
-        };
+const renderBarBackground = (progress, options) => {
+    if (progress > 1) {
+        return `repeating-linear-gradient(135deg, ${options.color}, ${
+            options.color
+        } 10px, #5e639a 10px, #5e639a 20px`;
+    } else {
+        return options.color;
     }
+};
 
-    renderBarBackground(progress, options) {
-        if (progress > 1) {
-            return `repeating-linear-gradient(135deg, ${options.color}, ${
-                options.color
-            } 10px, #5e639a 10px, #5e639a 20px`;
-        } else {
-            return options.color;
-        }
-    }
-
-    render() {
-        const { options, progress } = this.props;
-
-        return (
-            <div style={{ width: `${options.containerStyle.width}`, margin: `${options.containerStyle.margin}` }}>
-                <Container style={{ height: `${options.height}px`, maxHeight: `${options.height}px`, width: '100%' }}>
-                    <Bar
-                        style={{
-                            height: '100%',
-                            width: progress > 1 ? '100%' : `${progress * 100}%`,
-                            background: this.renderBarBackground(progress, options),
-                            marginBottom: `-${options.height}px`,
-                            zIndex: '2'
-                        }}
-                    />
-                    <Bar
-                        style={{
-                            width: '100%',
-                            height: `${options.height}px`,
-                            background: options.trailColor
-                        }}
-                    />
-                </Container>
-                <Text style={options.text.style}>{options.text.value}</Text>
-            </div>
-        );
-    }
-}
+const ProgressBar = ({ options, progress }) => (
+    <Container width={options.containerStyle.width} margin={options.containerStyle.margin}>
+        <BarWrapper height={options.height}>
+            <Bar
+                style={{
+                    height: '100%',
+                    width: progress > 1 ? '100%' : `${progress * 100}%`,
+                    background: renderBarBackground(progress, options),
+                    marginBottom: `-${options.height}px`,
+                    zIndex: '2'
+                }}
+            />
+            <Bar
+                style={{
+                    width: '100%',
+                    height: `${options.height}px`,
+                    background: options.trailColor
+                }}
+            />
+        </BarWrapper>
+        <Text style={options.text.style}>{options.text.value}</Text>
+    </Container>
+);
 
 export default ProgressBar;
