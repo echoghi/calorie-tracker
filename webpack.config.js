af00e8fd07da8fbff2235f691e47a114df98769c
emilec <emchoghi@gmail.com>
2019-05-18 18:38:35 -0700

    manifest.json

--- webpack.config.js
+++ webpack.config.js
@@ -300,29 +300,10 @@ module.exports = function(env, argv) {
                 })
         ].filter(Boolean),
 
-        // split out each module
-        // https://hackernoon.com/the-100-correct-way-to-split-your-chunks-with-webpack-f8a9df5b7758
+        // split out vendor js into its own bundle
         optimization: {
-            runtimeChunk: 'single',
             splitChunks: {
-                chunks: 'all',
-                maxInitialRequests: Infinity,
-                minSize: 0,
-                cacheGroups: {
-                    vendor: {
-                        test: /[\\/]node_modules[\\/]/,
-                        name(module) {
-                            // get the name. E.g. node_modules/packageName/not/this/part.js
-                            // or node_modules/packageName
-                            const packageName = module.context.match(
-                                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
-                            )[1];
-
-                            // npm package names are URL-safe, but some servers don't like @ symbols
-                            return `npm.${packageName.replace('@', '')}`;
-                        }
-                    }
-                }
+                chunks: 'all'
             }
         },
 
