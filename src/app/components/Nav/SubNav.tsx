41e3532941928d2ad7de6df051067e5c6bad2571
emilec <emchoghi@gmail.com>
2019-05-27 17:51:27 -0700

    subnav indicator fix

--- src/app/components/Nav/SubNav.tsx
+++ src/app/components/Nav/SubNav.tsx
@@ -3,31 +3,31 @@ import { SubNavMenu as Menu, SubNavIcon as Icon, PageIndicator } from './styles'
 import { NavLink } from 'react-router-dom';
 import Fade from '@material-ui/core/Fade';
 
-const SubNav = () => {
-    const Indicator = (name: string) =>
-        window.location.pathname === `/${name}` && (
-            <Fade in={true}>
-                <PageIndicator>.</PageIndicator>
-            </Fade>
-        );
+const Indicator = ({ name = '' }: { name?: string }) =>
+    window.location.pathname === `/${name}` && (
+        <Fade in={true}>
+            <PageIndicator>.</PageIndicator>
+        </Fade>
+    );
 
+const SubNav = () => {
     return (
         <Menu>
             <NavLink exact={true} to="/" activeClassName="active">
                 <Icon className="icon-calendar" />
                 <li>Calendar</li>
-                {Indicator('calendar')}
+                <Indicator />
             </NavLink>
             <NavLink to="/nutrition" activeClassName="active">
                 <Icon className="icon-bar-chart" />
                 <li>Nutrition</li>
-                {Indicator('nutrition')}
+                <Indicator name="nutrition" />
             </NavLink>
 
             <NavLink to="/settings" activeClassName="active">
                 <Icon className="icon-settings" />
                 <li>Settings</li>
-                {Indicator('settings')}
+                <Indicator name="settings" />
             </NavLink>
         </Menu>
     );
